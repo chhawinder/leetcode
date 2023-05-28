e47ed6d4a972329349ca8bcf6e922cbbf0dbd419
chhawinder <96365487+chhawinder@users.noreply.github.com>
2023-05-28 17:55:16 +0530

    Time: 16 ms (12.12%), Space: 14.4 MB (16.82%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,31 +1,21 @@
 class Solution {
-    bool isValid(int nrow, int ncol, int N,int m) {
-        return nrow >= 0 && nrow < N && ncol >= 0 && ncol < m;
-    }
 public:
+    void dfs(vector<vector<int>>& image, int sr, int sc,vector<vector<int>>&vis, int color,int ini){
+        vis[sr][sc]=1;
+        image[sr][sc]=color;
+        int dx[]={0,0,-1,1};
+        int dy[]={-1,1,0,0};
+        for(int i=0;i<4;i++){
+            int nr=sr+dx[i];
+            int nc=sc+dy[i];
+            if(nr<0||nc<0||nr>=image.size()||nc>=image[0].size())continue;
+            if(vis[nr][nc]==0&&image[nr][nc]==ini)dfs(image,nr,nc,vis,color,ini);
+        }
+    }
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
         vector<vector<int>>vis(image.size(),vector<int>(image[0].size(),0));
-        queue<pair<int,int>>q;
-        q.push({sr,sc});
-        while(q.size()!=0){
-            int r=q.front().first;
-            int c=q.front().second;
-            vis[r][c]=1;
-            q.pop();
-            int dr[]={0,-1,0,1};
-            int dc[]={-1,0,1,0};
-            for(int i=0;i<4;i++){
-                int nr=dr[i]+r;
-                int nc=dc[i]+c;
-                if(isValid(nr,nc,image.size(),image[0].size())&&vis[nr][nc]==0&&image[nr][nc]==image[r][c]){
-                    q.push({nr,nc});
-                    
-                }
-                
-            }
-            image[r][c]=color;
-            
-        }
+        int ini=image[sr][sc];
+        dfs(image,sr,sc,vis,color,ini);
         return image;
     }
 };

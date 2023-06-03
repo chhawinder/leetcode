d419924282d5765ccdc9f6c9120781a0ab3ea6fe
chhawinder <96365487+chhawinder@users.noreply.github.com>
2023-06-03 19:34:35 +0530

    Added solution - LeetHub

--- Distance of nearest cell having 1 - GFG/distance-of-nearest-cell-having-1.cpp
+++ Distance of nearest cell having 1 - GFG/distance-of-nearest-cell-having-1.cpp
@@ -7,46 +7,46 @@ class Solution
 {
     public:
     //Function to find distance of nearest 1 in the grid for each cell.
-    bool valid(int i,int j,int n,int m){
-        if(i<0||j<0||i>=n||j>=m)return false;
-        return true;
-    }
 	vector<vector<int>>nearest(vector<vector<int>>grid)
 	{
 	    // Code here
-	    int row=grid.size();
-	    int col=grid[0].size();
+	    int n=grid.size();
+	    int m=grid[0].size();
+	    vector<vector<int>>res(n,vector<int>(m,-1));
 	    queue<pair<int,int>>q;
-	    vector<vector<int>>ans(row,vector<int>(col,-1));
-	    for(int i=0;i<row;i++){
-	        for(int j=0;j<col;j++){
+	    for(int i=0;i<n;i++){
+	        for(int j=0;j<m;j++){
 	            if(grid[i][j]==1){
+	                res[i][j]=0;
 	                q.push({i,j});
-	                ans[i][j]=0;
 	            }
 	        }
 	    }
+	    
 	    int dx[]={0,0,-1,1};
 	    int dy[]={-1,1,0,0};
-	    int level=0;
-	    while(q.size()!=0){
-	        int size=q.size();
-	        for(int i=0;i<size;i++){
-	            int a=q.front().first;
-	            int b=q.front().second;
-	            q.pop();
-	            for(int k=0;k<4;k++){
-	                int na=a+dx[k];
-	                int nb=b+dy[k];
-	                if(na<0||nb<0||na>=row||nb>=col||ans[na][nb]!=-1) continue;
-	                q.push({na,nb});
-	                ans[na][nb]=ans[a][b]+1;
+	    
+	    while(q.size()){
+	        int row=q.front().first;
+	        int col=q.front().second;q.pop();
+	        
+	        for(int k=0;k<4;k++){
+	            int nr=row+dx[k];
+	            int nc=col+dy[k];
+	            if(nc<0||nr<0||nr>=n||nc>=m)continue;
+	            if(grid[nr][nc]==0){
+	                if(res[nr][nc]==-1){
+	                    res[nr][nc]=res[row][col]+1;
+	                    q.push({nr,nc});
+	                }else{
+	                    res[nr][nc]=min(res[nr][nc],res[row][col]+1);
+	                }
 	            }
 	        }
-	    }
-	    return ans;
+	        
+	    }return res;
+	    
 	}
-
 };
 
 //{ Driver Code Starts.

6e208bee81d22663d46488ff1ccc8d7b9512b5a1
chhawinder <96365487+chhawinder@users.noreply.github.com>
2023-06-03 18:48:00 +0530

    Time: 322 ms (60.63%), Space: 122.1 MB (49.64%) - LeetHub

--- 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.cpp
+++ 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.cpp
@@ -1,27 +1,19 @@
 class Solution {
 public:
+    int help(vector<vector<int>>&tree,int index,vector<int>& it){
+        int res=0;
+        for(auto i:tree[index]){
+            res=max(res,help(tree,i,it));
+        }
+        return res+it[index];
+    }
     int numOfMinutes(int n, int headID, vector<int>& manager, vector<int>& informTime) {
-        unordered_map<int,vector<int>>m;
-                for(int i=0;i<manager.size();i++) {
-            m[manager[i]].push_back(i);
+        vector<vector<int>>tree(n);
+        for(int i=0;i<manager.size();i++){
+            if(manager[i]!=-1)
+            tree[manager[i]].push_back(i);
         }
-        queue<pair<int,int>>q;
-        q.push({headID,0});
-        int maxi=0;
-        while(q.size()!=0){
-            int n=q.size();
-            for(int i=0;i<n;i++){
-                int head=q.front().first;
-                int timetaken=q.front().second;
-                q.pop();
-                int dur=informTime[head]+timetaken;
-                maxi=max(maxi,dur);
-                for(auto it:m[head]){
-                    q.push({it,dur});
-                }
-            }
-        }
-        return maxi;
-
+        return help(tree,headID,informTime);
+        
     }
 };

7b39bdd99c2255971d5b4c8b5f997306e5001e10
chhawinder <96365487+chhawinder@users.noreply.github.com>
2023-06-04 07:36:51 +0530

    Time: 20 ms (92.44%), Space: 14.7 MB (40.34%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,40 +1,39 @@
 class Solution {
 public:
-   int findCircleNum(vector<vector<int>>& isConnected) {
-    vector<vector<int>> adj(isConnected.size());
-    for (int i = 0; i < isConnected.size(); i++) {
-        for (int j = 0; j < isConnected[i].size(); j++) {
-            if (i != j && isConnected[i][j] == 1) {
-                adj[i].push_back(j);
+    int findCircleNum(vector<vector<int>>& isConnected) {
+        vector<vector<int>>adj(isConnected.size()+1);
+        for(int i=0;i<isConnected.size();i++){
+            for(int j=0;j<isConnected.size();j++){
+                if(i!=j&&isConnected[i][j]==1){
+                    adj[i+1].push_back(j+1);
+                }
             }
         }
-    }
-    
-    vector<int> vis(isConnected.size(), 0);
-    int ans = 0;
-    
-    for (int i = 0; i < isConnected.size(); i++) {
-        if (vis[i] == 0) {
-            ans++;
-            queue<int> q;
-            q.push(i);
-            vis[i] = 1;
-
-            while (!q.empty()) {
-                int t = q.front();
-                q.pop();
-
-                for (auto it : adj[t]) {
-                    if (vis[it] == 0) {
-                        q.push(it);
-                        vis[it] = 1;
+        int province=1;
+        queue<int>q;
+        vector<int>vis(isConnected.size()+1,0);
+        q.push(1);
+        vis[1]=1;
+        while(q.size()){
+            int temp=q.front();
+            q.pop();
+            for(auto it:adj[temp]){
+                if(vis[it]==0){
+                    vis[it]=1;
+                    q.push(it);
+                }
+            }
+            if(q.size()==0){
+                for(int i=1;i<=isConnected.size();i++){
+                    if(vis[i]==0){
+                        vis[i]=1;
+                        q.push(i);
+                        province++;
+                        break;
                     }
                 }
             }
         }
+        return province;
     }
-    
-    return ans;
-}
-
 };

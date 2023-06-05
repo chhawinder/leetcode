4e7991af438a07cc501f719226264205c59cb7ff
chhawinder <96365487+chhawinder@users.noreply.github.com>
2023-06-05 12:09:31 +0530

    Time: 46 ms (32.88%), Space: 19.2 MB (15.44%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,41 +1,35 @@
 class Solution {
 public:
     int numIslands(vector<vector<char>>& grid) {
-        int n = grid.size();
-        int m = grid[0].size();
-        vector<vector<bool>> visited(n, vector<bool>(m, false));
-        int numIslands = 0;
-
-        int dx[] = {0, 0, -1, 1};
-        int dy[] = {-1, 1, 0, 0};
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (grid[i][j] == '1' && !visited[i][j]) {
-                    numIslands++;
-                    queue<pair<int, int>> q;
-                    q.push({i, j});
-                    visited[i][j] = true;
-
-                    while (!q.empty()) {
-                        int x = q.front().first;
-                        int y = q.front().second;
+        int n=grid.size(),m=grid[0].size();
+        vector<vector<int>>vis(n,vector<int>(m,0));
+        int num=0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j]=='1'&&vis[i][j]==0){
+                    queue<pair<int,int>>q;
+                    num++;
+                    vis[i][j]=1;
+                    q.push({i,j});
+                    int dx[]={0,0,-1,1};
+                    int dy[]={-1,1,0,0};
+                    while(q.size()){
+                        int r=q.front().first;
+                        int c=q.front().second;
                         q.pop();
-
-                        for (int k = 0; k < 4; k++) {
-                            int nx = x + dx[k];
-                            int ny = y + dy[k];
-
-                            if (nx >= 0 && nx < n && ny >= 0 && ny < m && grid[nx][ny] == '1' && !visited[nx][ny]) {
-                                q.push({nx, ny});
-                                visited[nx][ny] = true;
+                        for(int k=0;k<4;k++){
+                            int nr=r+dx[k];
+                            int nc=c+dy[k];
+                            if(nr<0||nc<0||nr>=n||nc>=m)continue;
+                            if(grid[nr][nc]=='1'&&vis[nr][nc]==0){
+                                vis[nr][nc]=1;
+                                q.push({nr,nc});
                             }
                         }
                     }
                 }
             }
         }
-
-        return numIslands;
+        return num;
     }
 };

0d669a4effaed9a979fe0fce7930603df0656cbc
chhawinder <96365487+chhawinder@users.noreply.github.com>
2023-07-03 20:33:42 +0530

    Time: 0 ms (100.00%), Space: 6.9 MB (87.16%) - LeetHub

---
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        //take transpose 
+        for(int i=0;i<matrix.size();i++){
+            for(int j=i+1;j<matrix[0].size();j++){
+                swap(matrix[i][j],matrix[j][i]);
+            }
+        }
+        
+        // reverse every vector
+        for(int i=0;i<matrix.size();i++){
+            reverse(matrix[i].begin(),matrix[i].end());
+        }
+        
+    }
+};

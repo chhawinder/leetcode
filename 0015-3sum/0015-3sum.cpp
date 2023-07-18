0ccba7c579c133af5ac5ab09a6378fbc5eccbc52
chhawinder <96365487+chhawinder@users.noreply.github.com>
2023-07-18 13:53:26 +0530

    Time: 171 ms (73.20%), Space: 24 MB (71.08%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,27 +1,25 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        set<vector<int>>s;
         sort(nums.begin(),nums.end());
-        for(int i=0;i<nums.size();i++){
+         vector<vector<int>>res;
+        for(int i=0;i<nums.size()-2;i++){
+            
+            while(i>0&&nums[i]==nums[i-1]&&i<nums.size()-2)i++;
+            
+            
             int j=i+1,k=nums.size()-1;
             while(j<k){
                 int sum=nums[i]+nums[j]+nums[k];
                 if(sum==0){
-                    s.insert({nums[i],nums[j],nums[k]});
+                    res.push_back({nums[i],nums[j],nums[k]});
+                    while(j<nums.size()-1&&nums[j]==nums[j+1])j++;
+                    while(k>0&&nums[k]==nums[k-1])k--;
                     j++;k--;
-                }else if(sum<0){
-                    j++;
-                }else{
-                    k--;
-                }
+                }else if(sum>0)k--;
+                else j++;
             }
-            
         }
-        vector<vector<int>>output;
-        for(auto it:s){
-            output.push_back(it);
-        }
-        return output;
+        return res;
     }
 };

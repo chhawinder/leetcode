269f83e9820d45b040b60d45c37ddabb32acfa66
chhawinder <96365487+chhawinder@users.noreply.github.com>
2023-08-19 09:48:47 +0530

    Time: 23 ms (87.41%), Space: 70.9 MB (94.07%) - LeetHub

---
+++ 0002-add-two-numbers/0002-add-two-numbers.cpp
@@ -0,0 +1,82 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reversell(ListNode* head) {
+        if(head==NULL||head->next==NULL)return head;
+        ListNode*prev=NULL;
+        ListNode*nxt=head->next;
+        while(head!=NULL){
+            head->next=prev;
+            prev=head;
+            head=nxt;
+            if(nxt!=NULL)
+            nxt=nxt->next;
+        }
+        return prev;
+    }
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        
+        ListNode*temp1=l1;
+        l1=temp1;
+        ListNode*temp2=l2;
+        int carry=0;
+        while(temp1&&temp2){
+            int sum=temp1->val+temp2->val+carry;
+            temp1->val=sum%10;
+            carry=sum/10;
+            temp1=temp1->next;temp2=temp2->next;
+        }
+        if(temp1){
+            ListNode*k=temp1;
+            while(carry&&temp1){
+                int sum=temp1->val+carry;
+                temp1->val=sum%10;
+                carry=sum/10;
+                temp1=temp1->next;
+            }
+//             if(carry){
+//                 while(k->next)k=k->next;
+//                 ListNode*newnode=new ListNode(carry);
+//                 k->next=newnode;
+                
+//             }
+            
+        }
+        if(temp2){
+            ListNode*k=l1;
+            while(k->next)k=k->next;
+            k->next=temp2;
+            while(carry&&temp2){
+                int sum=temp2->val+carry;
+                temp2->val=sum%10;
+                carry=sum/10;
+                temp2=temp2->next;
+            }
+            
+//             if(carry){
+//                 while(k->next)k=k->next;
+//                 ListNode*newnode=new ListNode(carry);
+//                 k->next=newnode;
+                
+//             }
+        }
+        
+        if(carry){
+                ListNode*k=l1;
+                while(k->next)k=k->next;
+                ListNode*newnode=new ListNode(carry);
+                k->next=newnode;
+                
+            }
+        return l1;
+    }
+};

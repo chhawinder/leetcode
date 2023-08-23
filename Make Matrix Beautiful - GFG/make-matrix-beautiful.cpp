3f2aa25cd9ee6ebad1e6054e6d6636bc34bb6f42
Chhawinderpreet Singh <96365487+chhawinder@users.noreply.github.com>
2023-08-23 05:40:25 +0530

    Added solution - LeetHub

--- Make Matrix Beautiful - GFG/make-matrix-beautiful.cpp
+++ Make Matrix Beautiful - GFG/make-matrix-beautiful.cpp
@@ -12,30 +12,37 @@ class Solution
     //to make the matrix beautiful.
     int findMinOpeartion(vector<vector<int> > matrix, int n)
     {
-        int a= 0,b = 0;
-         for(int i = 0;i<n;i++)
-         {
-            int colsum = 0, rowsum = 0;
-             for(int j = 0;j<n;j++)
-             {
-                 colsum += matrix[i][j];
-                 rowsum += matrix[j][i];
-             }
-             a = max(a,colsum);
-             b = max(b,rowsum);
-         }
-         a = max(a,b);
-         int ans = 0;
-         for(int i = 0;i<n;i++)
-         {
-             int sum1 = 0;
-             for(int j = 0;j<n;j++)
-             {
-                 sum1 += matrix[i][j];
-             }
-             ans += abs(a-sum1);
-         }
-         return ans;
+        vector<int>row(matrix.size());int m=INT_MIN;
+        for(int i=0;i<matrix.size();i++){
+            int sum=0;
+            for(int j=0;j<matrix[0].size();j++){
+                sum+=matrix[i][j];
+            }
+            row[i]=sum;
+            m=max(m,sum);
+        }
+        int res=0;
+        for(int i=0;i<matrix.size();i++){
+            res+=m-row[i];
+        }
+        
+        vector<int>col(matrix[0].size());int m2=INT_MIN;
+        for(int i=0;i<matrix[0].size();i++){
+            int sum=0;
+            for(int j=0;j<matrix.size();j++){
+                sum+=matrix[j][i];
+            }
+            col[i]=sum;
+            // cout<<col[i]<<" ";
+            m2=max(m2,sum);
+        }
+        
+        int res2=0;
+        for(int i=0;i<matrix.size();i++){
+            res2+=m2-col[i];
+        }
+        return max(res,res2);
+        // code here 
     } 
 };
 

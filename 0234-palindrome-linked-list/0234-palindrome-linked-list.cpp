a8a736eb001c916cff6052302eae0050d2101f1e
Chhawinderpreet Singh <96365487+chhawinder@users.noreply.github.com>
2023-11-08 12:40:30 +0530

    Time: 204 ms (17.60%), Space: 122.3 MB (34.77%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
@@ -10,33 +10,42 @@
  */
 class Solution {
 public:
-    ListNode* reverse(ListNode* n){
-        ListNode*prev=NULL;
-        ListNode*curr=n;
-        ListNode*nx=n->next;
-        while(curr!=NULL){
-            curr->next=prev;
-            prev=curr;
-            curr=nx;
-            if(nx!=NULL)
-            nx=nx->next;
-        }
-        return prev;
-    }
+    // ListNode* reverse(ListNode* n){
+    //     ListNode*prev=NULL;
+    //     ListNode*curr=n;
+    //     ListNode*nx=n->next;
+    //     while(curr!=NULL){
+    //         curr->next=prev;
+    //         prev=curr;
+    //         curr=nx;
+    //         if(nx!=NULL)
+    //         nx=nx->next;
+    //     }
+    //     return prev;
+    // }
     bool isPalindrome(ListNode* head) {
-        ListNode*temp=head;
-        vector<int>c;
-        while(temp!=NULL){
-            c.push_back(temp->val);
-            temp=temp->next;
-        }
-        temp=reverse(head);
-        vector<int>p;
-        while(temp!=NULL){
-            p.push_back(temp->val);
-            temp=temp->next;
-        }
-        if(p==c)return true;
-        return false;
+        // ListNode*temp=head;
+        // vector<int>c;
+        // while(temp!=NULL){
+        //     c.push_back(temp->val);
+        //     temp=temp->next;
+        // }
+        // temp=reverse(head);
+        // vector<int>p;
+        // while(temp!=NULL){
+        //     p.push_back(temp->val);
+        //     temp=temp->next;
+        // }
+        // if(p==c)return true;
+        // return false;
+        
+        string s="";
+    while(head){
+        s+=head->val;
+        head=head->next;
+    }
+    string k=s;
+    reverse(s.begin(),s.end());
+    return s==k;
     }
 };

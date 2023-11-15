4ae1c19409b9c01428d9a072bccef1d9dfffafa6
Chhawinderpreet Singh <96365487+chhawinder@users.noreply.github.com>
2023-11-15 12:21:28 +0530

    Time: 86 ms (12.08%), Space: 73.5 MB (6.18%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,25 +1,28 @@
 class Solution {
 public:
-    vector<vector<int>>ans;
-    void help(vector<int>& c,int target,vector<int>&res,int  i){
-        if(i==c.size()&&target==0){
-            ans.push_back(res);
-            return;}
-        if(i==c.size()||target<0)return;
-        help(c,target,res,i+1);
-        if(target-c[i]>=0){
-            res.push_back(c[i]);
-            help(c,target-c[i],res,i);
-            res.pop_back();
-                
-        }
-        
+    void help(vector<int> arr, int i,int k,set<vector<int>>&sol,vector<int> sub){
+    if(k==0){
+        sol.insert(sub);
         
     }
-    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        vector<int>res;
-        help(candidates,target,res,0);
-        return ans;
+    if(i==arr.size())return ;
+    //ntake
+    help(arr,i+1,k,sol,sub);
+    //take
+    if(k>=arr[i]){
+        sub.push_back(arr[i]);
+        help(arr,i,k-arr[i],sol,sub);
+    }
         
+    
+}
+    vector<vector<int>> combinationSum(vector<int>& arr, int k) {
+        set<vector<int>>sol;
+    help(arr, 0, k, sol, {});
+    vector<vector<int>>res;
+    for(auto it:sol){
+        res.push_back(it);
+    }
+    return res;
     }
 };

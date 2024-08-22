e047e5a1883b09dc385f8141e3d8c13f481b949a
Chhawinderpreet Singh <96365487+chhawinder@users.noreply.github.com>
2024-08-22 17:35:33 +0530

    Time: 0 ms (100.00%), Space: 7.5 MB (33.08%) - LeetHub

---
+++ 0476-number-complement/0476-number-complement.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int findComplement(int num) {
+        int ans = 0;
+        int i = 0;
+        while(num != 0){
+            if(!(num & 1)) ans += pow(2, i);
+            i++;
+            num = num >> 1;
+        }
+        return ans;
+        
+    }
+};
